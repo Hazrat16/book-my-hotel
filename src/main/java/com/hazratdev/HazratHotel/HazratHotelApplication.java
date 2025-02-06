8902e6fbdcd7521d22f72d46b1662dab5476ac6f
hazrat <hazrat17016@gmail.com>
2025-02-06 10:37:46 +0600

    Initializing the hotel booking app

---
+++ src/main/java/com/hazratdev/HazratHotel/HazratHotelApplication.java
@@ -0,0 +1,13 @@
+package com.hazratdev.HazratHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazratHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazratHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hazratdev/HazratHotel/HazratHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazratdev.HazratHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazratHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
