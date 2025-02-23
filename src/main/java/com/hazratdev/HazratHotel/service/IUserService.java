ef38b904a436b47b77a4cad9aff0869a78d64e6e
hazrat <hazrat17016@gmail.com>
2025-02-23 19:43:15 +0600

    interface added

---
+++ src/main/java/com/hazratdev/HazratHotel/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.hazratdev.HazratHotel.service;
+
+import com.hazratdev.HazratHotel.dto.LoginRequest;
+import com.hazratdev.HazratHotel.dto.Response;
+import com.hazratdev.HazratHotel.entity.User;
+
+public interface IUserService {
+
+    Response register(User user);
+    Response login(LoginRequest loginRequest);
+    Response getAllUsers();
+    Response getUserBookingHistory(String userId);
+    Response deleteUser(String userId);
+    Response getUserById(String userId);
+    Response getMyInfo(String email);
+}
