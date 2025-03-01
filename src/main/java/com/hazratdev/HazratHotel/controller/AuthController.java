cfd69a261783a2e775d4bc6587cccc6db61f7533
hazrat <hazrat17016@gmail.com>
2025-03-01 21:17:48 +0600

    auth controller added

---
+++ src/main/java/com/hazratdev/HazratHotel/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.hazratdev.HazratHotel.controller;
+
+import com.hazratdev.HazratHotel.dto.LoginRequest;
+import com.hazratdev.HazratHotel.dto.Response;
+import com.hazratdev.HazratHotel.entity.User;
+import com.hazratdev.HazratHotel.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody  User user) {
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest) {
+        Response response = userService.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+
+    }
+}
