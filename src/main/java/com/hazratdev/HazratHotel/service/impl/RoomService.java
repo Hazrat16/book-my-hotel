463b0801cefda212b2de3d7b96f2db0ea6ceed29
hazrat <hazrat17016@gmail.com>
2025-03-08 21:41:28 +0600

    update service has been introduced

--- src/main/java/com/hazratdev/HazratHotel/service/impl/RoomService.java
+++ src/main/java/com/hazratdev/HazratHotel/service/impl/RoomService.java
@@ -102,7 +102,32 @@ public class RoomService implements IRoomService {
 
     @Override
     public Response updateRoom(Long roomId, String description, String roomType, BigDecimal roomPrice, MultipartFile photo) {
-        return null;
+        Response response = new Response();
+        try {
+            String imageUrl = null;
+
+            if(photo != null && !photo.isEmpty()) {
+                imageUrl = awsS3Service.saveImageToS3(photo);
+            }
+            Room room = roomRepository.findById(roomId).orElseThrow(() -> new OurException("Room not found"));
+            if(roomType != null) room.setRoomType(roomType);
+            if(roomPrice != null) room.setRoomPrice(roomPrice);
+            if(imageUrl != null) room.setRoomPhotoUrl(imageUrl);
+            if(description != null) room.setRoomDescription(description);
+            Room updatedRoom = roomRepository.save(room);
+            RoomDTO roomDTO = Utils.mapRoomEntityToRoomDTO(updatedRoom);
+            response.setStatusCode(200);
+            response.setMessage("Room updated successfully");
+            response.setRoom(roomDTO);
+
+        }catch (OurException e){
+            response.setStatusCode(400);
+            response.setMessage(e.getMessage());
+
+        }
+
+
+        return response;
     }
 
     @Override
